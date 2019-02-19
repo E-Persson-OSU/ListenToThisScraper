d31509e3a5a984238a99530bc2d37cfd4889902e
Persson <osuepersson@gmail.com>
2019-02-19 10:09:10 -0500

    Bot is functional

--- ListenToThisScraper.py
+++ ListenToThisScraper.py
@@ -1,15 +1,16 @@
 #ListenToThisScraper.py
 
+import http.server
+import os
+import socketserver
+import sqlite3
+import time
+from configparser import ConfigParser
+
 #imports
 import praw
-import sqlite3
 import spotipy
 import spotipy.util as util
-import time
-import os
-import http.server
-import socketserver
-from configparser import ConfigParser
 
 #Flow
 #connect to praw and spotify
@@ -43,17 +44,17 @@ rp = cfg['reddit']['password']           #Reddit Password
 ru = cfg['reddit']['username']           #Reddit Username
 
 #Spotify Info
-sci = cfg['spotify']['client_id']       #Spotify Client ID
-scs = cfg['spotify']['client_secret']   #Spotify Client Secret
-sp = cfg['spotify']['password']         #Spotify Password
-su = cfg['spotify']['username']         #Spotify Username
-st = cfg['spotify']['token']            #Spotify Token
-spid = cfg['spotify']['playlist_id']    #Spotify Playlist ID
+sci = cfg['spotify']['client_id']        #Spotify Client ID
+scs = cfg['spotify']['client_secret']    #Spotify Client Secret
+sp = cfg['spotify']['password']          #Spotify Password
+su = cfg['spotify']['username']          #Spotify Username
+st = cfg['spotify']['token']             #Spotify Token
+spid = cfg['spotify']['playlist_id']     #Spotify Playlist ID
 scope = 'playlist-modify-public'
 redirect_uri = cfg['spotify']['redirect_uri']
 
 #Running Info
-rfp = cfg['files']['removed_songs']     #Removed Songs File Path
+rfp = cfg['files']['removed_songsfp']      #Removed Songs File Path
 
 
 #-----------Private Functions-----------------
@@ -79,14 +80,37 @@ def scrapel2t(reddit):
     l2t = reddit.subreddit('listentothis')
     for submission in l2t.hot(limit=25):
         songs.append(submission.title)
+    songs.pop(0)
     return songs
 
 def convertospotify(songstoadd,token):
     spt = spotipy.Spotify(auth=token)
+    spotifysonglist = []
     for song in songstoadd:
+        try:
+            try:
+                result = spt.search(q=str(song).split('-')[1].split(' [')[0], limit=1, type='track')
+            except spotipy.client.SpotifyException as spte:
+                print(str(spte))
+        except IndexError as ie:
+            print(str(ie))
+        try:
+            spotifysonglist.append(result['tracks']['items'][0]['id'])
+        except IndexError as ie:
+            print(str(ie))
+    return spotifysonglist
         
 
+#Add Songs to Spotify Playlist
+def addsongstoplaylist(playlist, token, spot): 
+    spot.trace = False
+    playlist = list(set(playlist))
+    results = spot.user_playlist_add_tracks(su, spid, playlist)
+    print(results)
 
+#clear playlist before adding songs to avoid dupes
+def emptyplaylist(spt, token, currenttracks):
+    spt.user_playlist_remove_all_occurrences_of_tracks(su, spid, currenttracks)
 
 #Check Spotify playlist
 def checkplaylist(token):
@@ -99,22 +123,41 @@ def checkplaylist(token):
     return pl
     
 #Compare currentsongs and addsongs to database, if old remove, compile both lists into one list
+    #currentsongs over the time limit need to be removed, addsongs need to be checked against the database for duplicates and removed
 def comparecurrent(currentsongs, addsongs):
     conn = sqlite3.connect(rfp)
     cur = conn.cursor() 
     addtime = time.time()
+    added = 'Initial Value'
+    #remove addsongs from currentsongs, might do something else with them later
+    for id in addsongs:
+        try:
+            currentsongs.remove(id)
+        except ValueError as ve:
+            print(id + ' not found in current songs.')
+    
+    #back to the script
     for id in currentsongs:
         try:
-            cur.execute('SELECT id FROM Tracks WHERE id=?', (id,))
+            cur.execute('SELECT added FROM Tracks WHERE id=?', (id,))
+            added = cur.fetchone()
+            if added == None:
+                print('No time found for ' + id)
+            elif int(addtime) - int(added) > 604800:
+                currentsongs.remove(id)
         except sqlite3.OperationalError:
             cur.execute('INSERT INTO Tracks (id, added) VALUES (?,?);', (id, time.time(),))
     for id in addsongs:
         try:
-            cur.execute('SELECT id FROM Tracks WHERE id=?', (id,))
+            check = cur.execute('SELECT id FROM Tracks WHERE id=?', (id,))
+            if check == id:
+                addsongs.remove(id)
         except sqlite3.OperationalError:
             cur.execute('INSERT INTO Tracks (id, added) VALUES (?,?);', (id, time.time(),))
     conn.close()
-    return songstoremove
+    for id in addsongs:
+        currentsongs.append(id)
+    return currentsongs
 
 #check list of removed songs
 #TODO make this check against database instead of text file
@@ -156,13 +199,15 @@ def main():
         spt,token = spotipyconnect(su, scope, sci, scs)
         removedsongfilecheck(rfp)
         addsongs = scrapel2t(reddit)
-        addsongs = convertospotify(addsongs)
+        addsongs = convertospotify(addsongs,token)
         addsongs = removedsongs(addsongs)
         currentsongs = checkplaylist(token)
+        emptyplaylist(spt, token, currentsongs)
         #TODO check database for songs to remove and remove
-        songstoremove = comparecurrent(currentsongs,addsongs)
-
+        newplaylist = comparecurrent(currentsongs,addsongs)
         #TODO add songs to playlist
+
+        addsongstoplaylist(newplaylist, token, spt)
         
         time.sleep(86400) #sleep for one day
         
@@ -171,4 +216,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
